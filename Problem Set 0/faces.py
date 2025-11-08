85883e6792104754df4c2c6c39a379e46b65a668
Richard Phan <richard.phan626@gmail.com>
2025-11-08 15:55:47 -0800

    Started on faces.py

---
+++ Problem Set 0/faces.py
@@ -0,0 +1,18 @@
+#Richard Phan
+#11/8/25
+
+#Goal: This program converts a string with an emoticon into a string with an emoji
+
+#function called convert, accept str as parameter
+    #RETURNS same input with emoticon converted to 🙂 or 🙁
+#All other text unchanged
+
+#main function
+    #prompt user for input
+
+    #calls convert
+
+    #prints result (set return value to a variable. print the variable)
+
+#call main
+
