4a33d45cc5a071c297da9f57bdcec2f08ecaf467
Richard Phan <richard.phan626@gmail.com>
2025-11-08 15:49:09 -0800

    Update indoor.py

--- Problem Set 0/indoor.py
+++ Problem Set 0/indoor.py
@@ -1,6 +1,14 @@
 # Richard Phan
 # 11/8/25
+"""
+Instructions: WRITING IN ALL CAPS IS LIKE YELLING.
 
+Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+
+In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+
+"""
 #Goal: this program takes a capitalized string and turns it into lowercase
 indoor_voice = str(input("Enter a sentence: "))
 print(indoor_voice.lower())
+
