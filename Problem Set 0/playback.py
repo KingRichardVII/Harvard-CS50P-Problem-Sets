1006398a891381bd86449cba10669b7655c50e0a
Richard Phan <richard.phan626@gmail.com>
2025-11-08 15:50:32 -0800

    uploaded playback.py

---
+++ Problem Set 0/playback.py
@@ -0,0 +1,7 @@
+# Richard Phan
+# 11/8/25
+
+#Goal: this program mimics a YT playback slowdown. Spaces in an inputted string is replaced with ...
+
+slowed_sentence = input("Enter a sentence: ")
+print(slowed_sentence.replace(" ", "...")) #.replace(old, new)
