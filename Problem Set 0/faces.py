a3491dba6526ae6e62ad8fdf784f22914d295421
Richard Phan <richard.phan626@gmail.com>
2025-11-08 16:05:26 -0800

    began working on faces.py

--- Problem Set 0/faces.py
+++ Problem Set 0/faces.py
@@ -4,15 +4,19 @@
 #Goal: This program converts a string with an emoticon into a string with an emoji
 
 #function called convert, accept str as parameter
+def convert(input_str):
     #RETURNS same input with emoticon converted to 🙂 or 🙁
+    return input_str.replace(":),🙂")
 #All other text unchanged
 
 #main function
+def main():
     #prompt user for input
-
+    hello_goodbye = input("Enter 'hello :)' or 'goodbye :(' ")
     #calls convert
-
+    convert(hello_goodbye)
     #prints result (set return value to a variable. print the variable)
-
+    result = convert(hello_goodbye)
+    print(result)
 #call main
-
+main()
