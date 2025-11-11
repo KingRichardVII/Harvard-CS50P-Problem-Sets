0701c901b4ded25c86863545b46545f1fe506825
Richard Phan <richard.phan626@gmail.com>
2025-11-10 22:39:22 -0800

    Finished tip.py problem
    Similar to tip calculator previously

--- Problem Set 0/tip.py
+++ Problem Set 0/tip.py
@@ -1,3 +1,10 @@
+# Richard Phan
+# 11/10/25
+
+#Goal: complete the following functions:
+#dollars to float - function which changes the data type from str to float
+#percent to float - function which changes 15% to a float 15.00, then 0.15
+
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
@@ -6,12 +13,11 @@ def main():
 
 
 def dollars_to_float(d):
-    pass
-
+    #replace() gets rid of $,  float changes data type of input
+    return float(d.replace("$", ""))
 
 def percent_to_float(p):
-
-    pass
+    return float(p.replace("%", "")) / 100 #divide by 100- reasoning: user inputs 15% not actually .15
 
 
 main()
