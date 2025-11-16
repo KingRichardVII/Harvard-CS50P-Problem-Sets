734926a131729e39f88b042b36c5a72e966ace5b
Richard Phan <richard.phan626@gmail.com>
2025-11-16 13:37:58 -0800

    solved deep.py

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -4,13 +4,14 @@
 #GOAL: prompt user for input. Outputs yes if user inputs 42 or forty-two or forty two. otherwise output no.
 
 def main():
-    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
 
-    if not (answer == "42" or answer == "forty-two" or answer == "forty two"):
+    if not (answer == "42" or answer == "forty-two" or answer == "forty two" or answer == "Forty Two"):
+        print("No")
+    else:
+        great_question(answer)
 
 
-
-def great_question():
-    print("Yes")
-
+def great_question(machine_answer):
+        print("Yes")
 main()
