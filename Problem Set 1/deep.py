5ff7e9305e893b15088f8918eb94f0c1e145f094
Richard Phan <richard.phan626@gmail.com>
2025-11-15 17:21:43 -0800

    Add files via upload

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,1 +1,16 @@
+# Richard Phan
+# 11/15/25
 
+#GOAL: prompt user for input. Outputs yes if user inputs 42 or forty-two or forty two. otherwise output no.
+
+def main():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+    if not (answer == "42" or answer == "forty-two" or answer == "forty two"):
+
+
+
+def great_question():
+    print("Yes")
+
+main()
