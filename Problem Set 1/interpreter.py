d58b6acdaa847b8f14f8b77a9bad87244ffa1d70
Richard Phan <richard.phan626@gmail.com>
2025-11-22 22:48:06 -0800

    started planning/psudeocode

---
+++ Problem Set 1/interpreter.py
@@ -0,0 +1,47 @@
+#Richard Phan
+#11/22/25
+
+"""
+GOAL:
+Python already supports math, whereby you can write code to add, subtract, multiply, or divide values and even variables. But let’s write a program that enables users to do math, even without knowing Python.
+
+In a file called interpreter.py, implement a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+
+x is an integer
+y is +, -, *, or /
+z is an integer
+For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
+
+Note that, just as python itself is an interpreter for Python, so will your interpreter.py be an interpreter for math!
+
+"""
+
+def main():
+    #variable x y z
+    #prompt the user for a math expression (float for float division)
+    #may require .split() string method
+
+    #if elif statement
+    #call add function
+
+    #call subtract function
+
+    #call subtract function
+
+    #call mult fcn
+
+    #call div fcn
+
+
+    #calculate result and round to 1 decimal place
+
+
+#all functions should have return value
+#def add
+
+#def subtract
+
+#def multipy
+
+#def divide
+main()
